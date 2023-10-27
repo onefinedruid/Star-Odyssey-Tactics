e9230b36ea4396f573874caa6f8d9298ec7ed1ea
Ian Weber <onefinedruid@gmail.com>
2023-10-27 19:20:34 -0400

    Refactor from Unity > Godot.

--- game_root/scripts/DragAndDropController.cs
+++ game_root/scripts/DragAndDropController.cs
@@ -1,30 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using Godot;
+using System;
 
-public class DragAndDropController : MonoBehaviour
+// To use this script:
+// 1.Create a Sprite node in your Godot 2D project.
+// 2.Attach this script to the Sprite node.
+// 3.Make sure the DragAndCropController script's properties are set correctly (e.g., the script should be placed in the Scripts tab of the Sprite node).
+// 4.Run your project, and you should be able to click and drag the sprite within the TileMap.
+
+public class DragAndCropController : Sprite
 {
-    public bool isDraggable = true;
-    private bool isDragged = false;
-    public Vector3 PositionMouse;
-    public Vector3 PositionObject;
-    void Update()
-    {
-        
-        PositionMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        PositionObject = transform.position;
+    private bool dragging = false;
+    private Vector2 offset;
 
-        if(isDragged) {
-            transform.position = new Vector3(Mathf.Round(PositionMouse.x/145)*145,Mathf.Round(PositionMouse.y/147)*147,0);
-            if(Input.GetMouseButtonUp(0)) {
-                isDragged=false;
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseEvent)
+        {
+            if (mouseEvent.ButtonIndex == (int)ButtonList.Left)
+            {
+                if (mouseEvent.Pressed)
+                {
+                    Vector2 mousePosition = GetGlobalMousePosition();
+                    if (RectMinSize.HasPoint(mousePosition))
+                    {
+                        // Start dragging when the left mouse button is pressed while the cursor is over the sprite
+                        dragging = true;
+                        offset = mousePosition - GlobalPosition;
+                        CaptureMouse();
+                    }
+                }
+                else if (dragging)
+                {
+                    // Stop dragging when the left mouse button is released
+                    dragging = false;
+                    ReleaseMouse();
+                }
             }
         }
     }
-    private void OnMouseDown() {
-        Debug.Log("Mouse Down");
-        if(isDraggable) {
-            isDragged=true;
+
+    public override void _Process(float delta)
+    {
+        if (dragging)
+        {
+            // Update the sprite's position based on the cursor position
+            GlobalPosition = GetGlobalMousePosition() - offset;
         }
     }
 }
+
